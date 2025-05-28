3170d6dd7e854714a6a8d25a08e3c4123d2cf167
Andrea Ambrose <andreamoesha@gmail.com>
2025-05-28 16:00:46 -0400

    empty header/app page

---
+++ calcvis/src/Components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header.jsx";
+
+export default function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <div className='Content'>
+        <p>42 is the answer to everything...</p>
+      </div>
+    </div>
+  );
+}
